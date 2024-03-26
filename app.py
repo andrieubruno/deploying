648d288ec660fcb324c347f78e59a04204cfd259
pittoul <pittoul@gmail.com>
2024-03-26 16:03:50 +0100

    dockerized

--- app.py
+++ app.py
@@ -12,4 +12,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    create_app.run(debug=True)
+    # create_app.run(debug=True)
+    create_app.run(host="0.0.0.0", port=5000, debug=True)
